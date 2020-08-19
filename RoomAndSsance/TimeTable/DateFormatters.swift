a522fb0a2727bcf00c16574c7370b3ad66c09b21
Yonghyun <lygon55555@gmail.com>
2020-08-19 11:19:39 +0900

    Date Formatter

---
+++ RoomAndSsance/TimeTable/DateFormatters.swift
@@ -0,0 +1,32 @@
+//
+//  DateFormatters.swift
+//  RoomAndSsance
+//
+//  Created by Yonghyun on 2020/08/18.
+//  Copyright © 2020 Yonghyun. All rights reserved.
+//
+
+import Foundation
+
+struct DateFormatters {
+  
+  static var shortDateFormatter: DateFormatter = {
+    let dateFormatter = DateFormatter()
+    dateFormatter.timeStyle = .none
+    dateFormatter.dateStyle = .short
+    return dateFormatter
+  }()
+  
+  static var dateFormatter: DateFormatter = {
+    let dateFormatter = DateFormatter()
+    dateFormatter.dateFormat = "d"
+    return dateFormatter
+  }()
+  
+  static var weekdayFormatter: DateFormatter = {
+    let dateFormatter = DateFormatter()
+    dateFormatter.dateFormat = "EEE"
+    return dateFormatter
+  }()
+  
+}
