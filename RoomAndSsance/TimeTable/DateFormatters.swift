53afecf0434345c7b95953a10c9d6f9fec9a7252
Yonghyun <lygon55555@gmail.com>
2020-08-20 15:15:03 +0900

    Date Formatter

--- RoomAndSsance/TimeTable/DateFormatters.swift
+++ RoomAndSsance/TimeTable/DateFormatters.swift
@@ -10,23 +10,34 @@ import Foundation
 
 struct DateFormatters {
   
-  static var shortDateFormatter: DateFormatter = {
-    let dateFormatter = DateFormatter()
-    dateFormatter.timeStyle = .none
-    dateFormatter.dateStyle = .short
-    return dateFormatter
-  }()
+    static var shortDateFormatter: DateFormatter = {
+        let dateFormatter = DateFormatter()
+        dateFormatter.timeStyle = .none
+        dateFormatter.dateStyle = .short
+        return dateFormatter
+    }()
+
+    static var dateFormatter: DateFormatter = {
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "d"
+        return dateFormatter
+    }()
+
+    static var weekdayFormatter: DateFormatter = {
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "EEE"
+        return dateFormatter
+    }()
+
+    static var monthFormatter: DateFormatter = {
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "LLLL"
+        return dateFormatter
+    }()
   
-  static var dateFormatter: DateFormatter = {
-    let dateFormatter = DateFormatter()
-    dateFormatter.dateFormat = "d"
-    return dateFormatter
-  }()
-  
-  static var weekdayFormatter: DateFormatter = {
-    let dateFormatter = DateFormatter()
-    dateFormatter.dateFormat = "EEE"
-    return dateFormatter
-  }()
-  
+    static var yearFormatter: DateFormatter = {
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "y"
+        return dateFormatter
+    }()
 }
