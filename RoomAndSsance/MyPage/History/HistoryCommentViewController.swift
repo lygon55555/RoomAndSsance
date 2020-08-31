da7ae977b6cff85e0313db09e513ab24839c52ea
Yonghyun <lygon55555@gmail.com>
2020-08-31 17:49:52 +0900

    My History View Controllers

---
+++ RoomAndSsance/MyPage/History/HistoryCommentViewController.swift
@@ -0,0 +1,14 @@
+//
+//  HistoryCommentViewController.swift
+//  RoomAndSsance
+//
+//  Created by Yonghyun on 2020/08/31.
+//  Copyright © 2020 Yonghyun. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class HistoryCommentViewController: UIViewController {
+    
+}
---
+++ RoomAndSsance/MyPage/History/HistoryViewController.swift
@@ -0,0 +1,15 @@
+//
+//  HistoryViewController.swift
+//  RoomAndSsance
+//
+//  Created by Yonghyun on 2020/08/31.
+//  Copyright © 2020 Yonghyun. All rights reserved.
+//
+
+import Foundation
+import UIKit
+import Parchment
+
+class HistoryViewController: UIViewController {
+    
+}
