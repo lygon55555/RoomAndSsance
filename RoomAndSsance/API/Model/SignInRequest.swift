ad68a71636c8e4bd0dc56e6eb93799d8d9ee1fa5
Yonghyun <lygon55555@gmail.com>
2020-09-09 10:54:16 +0900

    Sign In Request Model

---
+++ RoomAndSsance/API/Model/SignInRequest.swift
@@ -0,0 +1,14 @@
+//
+//  SignInRequest.swift
+//  RoomAndSsance
+//
+//  Created by Yonghyun on 2020/09/09.
+//  Copyright © 2020 Yonghyun. All rights reserved.
+//
+
+import Foundation
+
+struct SignInRequest: Codable {
+    let userId: String?
+    let password: String?
+}
